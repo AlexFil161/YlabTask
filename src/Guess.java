82274bb51bcf8fb91bf04d08fae9abf6a1339e8c
AlexFil161 <alexandr.filatov.98@mail.ru>
2023-03-06 16:21:01 +0300

    Initial commit

---
+++ src/Guess.java
@@ -0,0 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guess {
+    public static void main(String[] args) throws Exception {
+        int number = new Random().nextInt(99) + 1;
+        int maxAttempts = 10;
+        System.out.println("Я загад число от 1 до 99. У тебя " + maxAttempts + " попыток угадать.");
+        try (Scanner scanner = new Scanner(System.in)) {
+            for (int count = 1; count <= maxAttempts; count++) {
+                if (scanner.nextInt() > number) {
+                    System.out.println("Моё число меньше! Осталось " + (maxAttempts - count) + " попыток.");
+                } else if (scanner.nextInt() < number) {
+                    System.out.println("Моё число больше! Осталось " + (maxAttempts - count) + " попыток.");
+                } else if (scanner.nextInt() == number) {
+                    System.out.println("Ты угадал с " + count + " попытки!");
+                    System.exit(0);
+                } else if (count == maxAttempts){
+                    System.out.println("Ты не угадал.");
+                }
+            }
+            System.out.println("Ты не угадал.");
+        }
+    }
+}
---
+++ src/Pell.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Pell {
+    public static void main(String[] args) throws Exception {
+        try(Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите целое чило в диапазоне: 0<= n <= 30");
+            int n = scanner.nextInt();
+            while (n < 0 || n > 30) {
+                System.out.println("Введите целое чило в диапазоне: 0<= n <= 30");
+                n = scanner.nextInt();
+            }
+            if (n <= 1) {
+                System.out.println(n);
+            }
+            int a = 0;
+            int b = 1;
+            for (int i = 2; i <= n; i++) {
+                int c = 2 * b + a;
+                a = b;
+                b = c;
+            }
+            System.out.println(b);
+        }
+    }
+}
